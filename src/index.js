731664c34acd572d86c7d3362a343d992648a886
Zelina <zelina_c@yahoo.com.mx>
2019-06-13 18:28:38 -0500

    revisa la estructura HTML

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
   let cifrar = () => {
-    let cadena = document.getElementById('tarea').value; // texto a codificar
+    let cadena = document.getElementById('tarea').value;              //cadena a cifrar 
     let offset = parseInt(document.getElementById('offset').value);
     let nuevaCadena = cipher.cifrado(cadena, offset);
-    document.getElementById('resultado').value = nuevaCadena;
+    document.getElementById('resultado').value = nuevaCadena;         //valor de la cadena de texto
+    // document.getElementById('resultado').innerHTML = nuevaCadena;  //para un parrafo
   }
 document.getElementById('boton-cifrar').addEventListener('click',cifrar);
