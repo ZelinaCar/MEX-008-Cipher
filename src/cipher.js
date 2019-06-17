b4f165dced9a59b9b666ed46546c60fb99d9f317
Zelina <zelina_c@yahoo.com.mx>
2019-06-17 18:18:44 -0500

    agrega la funcion decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,30 @@
 window.cipher = {
 
-  cifrado: (string, offset) => {
+  encode: (string, offset) => {
     
+    string = string.toUpperCase();
     let nuevaCadena = "";
-    
+    // let nuevaCadena =  [];
     for (let i = 0; i < string.length; i++) {
-          let positionAscii = string.charCodeAt(i);
-          let newPositionAscii = ((positionAscii - 65 + offset) % 26 + 65);
-          let newCharacter = String.fromCharCode(newPositionAscii);
-          nuevaCadena = nuevaCadena.concat(newCharacter); 
-      }
-    return nuevaCadena;
+         let positionAscii = string.charCodeAt(i);
+         let newPositionAscii = ((positionAscii - 65 + offset) % 26 + 65);
+         let newCharacter = String.fromCharCode(newPositionAscii);
+         nuevaCadena = nuevaCadena.concat(newCharacter); 
+         // nuevaCadena.push(newCharacter); //devuelve el array separado por comas
     }
+  return nuevaCadena;
+  },
+
+  decode: (string, offset) => {
+    //string = string.toUpperCase();
+    let nuevaCadena = "";
+    for (let i = 0; i < string.lenght; i++) {
+         let positionAscii = string.charCodeAt(i);
+         let newPositionAscii = ((positionAscii + 65 - offset) % 26 + 65);
+         let newCharacter = String.fromCharCode(newPositionAscii);
+         nuevaCadena = nuevaCadena.concat(newCharacter); 
+    }
+  return nuevaCadena;
+  }
 
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 
-  let cifrar = () => {
-    let cadena = document.getElementById('tarea').value;              //cadena a cifrar 
-    let offset = parseInt(document.getElementById('offset').value);
-    let nuevaCadena = cipher.cifrado(cadena, offset);
-    document.getElementById('resultado').value = nuevaCadena;         //valor de la cadena de texto
-    // document.getElementById('resultado').innerHTML = nuevaCadena;  //para un parrafo
-  }
+
+let cifrar = () => {
+  let cadena = document.getElementById('tarea').value;              //cadena a cifrar 
+  let offset = parseInt(document.getElementById('offset').value);
+  let nuevaCadena = cipher.encode(cadena, offset);
+  document.getElementById('resultado').value = nuevaCadena;         //valor de la cadena de texto
+  // document.getElementById('resultado').innerHTML = nuevaCadena;  //para un parrafo
+}
 document.getElementById('boton-cifrar').addEventListener('click',cifrar);
+
+
+let descifrar = () => {
+  let cadena = document.getElementById('tarea').value;              //cadena a descifrar 
+  let offset = parseInt(document.getElementById('offset').value);
+  let nuevaCadena = cipher.decode(cadena, offset);
+  document.getElementById('resultado').value = nuevaCadena;         //valor de la cadena de texto
+  // document.getElementById('resultado').innerHTML = nuevaCadena;  //para un parrafo
+}
+document.getElementById('boton-descifrar').addEventListener('click',descifrar);
